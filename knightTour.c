c7188c3a37801d247f4540783922d8523b6c58e8
hhaus532 <hhaus532@pyrite-n3.cs.iastate.edu>
2024-01-23 13:03:55 -0600

    test

--- knightTour.c
+++ knightTour.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 #define MAX_INDEX 5
-#define b[25]
+#define MAX_ANSWER 25
 
 int main(int argc, char *argv[]){
     int A[MAX_INDEX][MAX_INDEX];
-    
+    int B[MAX_ANSWER];
+
+    return 0;
 }
