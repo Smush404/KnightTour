9bc84dc12d714e53741954c0ac77e0a320b6e095
Haakon H <hhaus532@iastate.edu>
2024-01-23 19:04:21 -0600

    my head is hurting

--- knightTour.c
+++ knightTour.c
@@ -3,60 +3,98 @@
 #define MAX_INDEX 5
 #define MAX_SIZE 25
 
-int start(int A[MAX_INDEX][MAX_INDEX], int B[1728][MAX_SIZE], int x, int y, int b){
-    if(x >= 0 && x <= 4 ||  y >= 0 && y <= 4 || b > 1728){
+int start(int A[MAX_INDEX][MAX_INDEX], int B[1728][MAX_SIZE], int x, int y, int b, int b_2){
+    if(x < 0 && x > 4 ||  y < 0 && y < 4 || b > 1728 || b_2 > 25){
         return 1; //error out of bounds
     }
 
-    for(int j = 0; j < sizeof(B[b]); j++){// no revists
+    for(int j = 0; j <= b_2; j++){// no revists
         if(A[x][y] == B[b][j]){
-            return 1;
+        
+            return 2; //found a repeat
         }
     }
 
-    if(sizeof(B[b]) != 25){
-        B[b][sizeof(B[b]) + 1] = A[x][y];
+    if(b_2 < 25){ //see if it can place the cell of A in B
+        B[b][b_2] = A[x][y];
     }
-    else{
-        b += 1;
-        B[b][sizeof(B[b]) + 1] = A[x][y];
+    else{ //b index is full 
+        b += 1; //increase the indexers
+        b_2 = 0;
+
+        if(b >= 1728){ //if B is full then start is done
+            return 0;
+        }
     }
+
+    b_2 += 1;
 
     if(x + 2 <= 4 && y + 1 <= 4){ //goes down and right
-        return start(A, B, x + 2, y + 1, b);
+        // int ret = start(A, B, x + 2, y + 1, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        //else{return 0;}
+        return start(A, B, x + 2, y + 1, b, b_2);
     }
-
-     if(x + 2 <= 4 && y - 1 >= 0){ //goes down and left
-        return start(A, B, x + 2, y - 1, b);
+    
+    if(x + 2 <= 4 && y - 1 >= 0){ //goes down and left
+        // int ret = start(A, B, x + 2, y - 1, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        return start(A, B, x + 2, y - 1, b, b_2);
     }
 
      if(x - 2 >= 0 && y + 1 <= 4){ //goes up and right
-        return start(A, B, x - 2, y + 1, b);
+        // int ret = start(A, B, x - 2, y + 1, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        return start(A, B, x - 2, y + 1, b, b_2);
     }
 
      if(x - 2 >= 0 && y - 1 >= 0){ //goes up and left
-        return start(A, B, x - 2, y - 1, b);
+        // int ret = start(A, B, x - 2, y - 1, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        return start(A, B, x - 2, y - 1, b, b_2);
     }
 
      if(x - 1 >= 0 && y - 2 >= 0){ //goes left and up
-        return start(A, B, x - 1, y - 2, b);
+        // int ret = start(A, B, x - 1, y - 2, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        return start(A, B, x - 1, y - 2, b, b_2);
     }
 
      if(x + 1 <= 4 && y - 2 >= 0){ //goes left and down
-        return start(A, B, x - 1, y - 2, b);
+        // int ret = start(A, B, x + 1, y - 2, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }
+        return start(A, B, x + 1, y - 2, b, b_2);
     }
 
     if(x - 1 >= 0 && y + 2 <= 4){ //goes right and up
-        return start(A, B, x - 1, y - 2, b);
+        // int ret = start(A, B, x - 1, y + 2, b, b_2);
+        // if (ret == 2 || ret == 3){
+        //     b_2 -= 1;
+        // }   
+        return start(A, B, x - 1, y + 2, b, b_2);
     }
 
-     if(x + 1 <= 4 && y + 2 <= 4){ //goes right and down
-        return start(A, B, x - 1, y - 2, b);
+    if(x + 1 <= 4 && y + 2 <= 4){ //goes right and down
+    // int ret = start(A, B, x + 2, y + 1, b, b_2);
+    //     if (ret == 2 || ret == 3){
+    //         b_2 -= 1;
+    //     }
+        return start(A, B, x + 1, y + 2, b, b_2);
     }
-
-
-
-    return 0;
+    
+    return 3; //found dead end
 }
 
 
@@ -72,10 +110,13 @@ void print_table(int A[MAX_INDEX][MAX_INDEX]){
 }
 
 int main(int argc, char *argv[]){
-    int A[MAX_INDEX][MAX_INDEX];
-    int B[1728][MAX_SIZE];
+
+    int A[MAX_INDEX][MAX_INDEX]; //matrix of the board
+    int B[1728][MAX_SIZE]; //array of all the answers
+
     int i, c = 1, b = 0; //i = index of current cell, c is a count to fill, b is what # tour we are on
     int x, y; //points on A
+
     for(i = 0; i < MAX_INDEX; i++){//fills the table
         for (int j = 0; j < MAX_INDEX; j++)
         {
@@ -84,9 +125,16 @@ int main(int argc, char *argv[]){
         }
     }
 
-    print_table(A);
+    for(i = 0; i < 20; i++){//fills the table
+        for (int j = 0; j < 25; j++)
+        {
+           B[i][j] = -1;
+        }
+    }
 
-    for(i = 0; i < MAX_SIZE; i++){
+    //print_table(A); //prints the table 
+
+    for(i = 0; i < MAX_SIZE; i++){ //finds the index of X and Y for each i
         if(i < 5){
             x = 0;
             y = i;
@@ -108,15 +156,16 @@ int main(int argc, char *argv[]){
             y = i - 20;
         }
 
-        start(A, B, x, y, b);
+        //printf("%3d", start(A, B, x, y, b, 0)); //calls the starting methods 
+        start(A, B, x, y, b, 0);
     }
 
-    for(int r = 0; r <= sizeof(B); r++){
-        for(int t = 0; t < sizeof(B[r]); t++){
+    for(int r = 0; r < 20; r++){
+        for(int t = 0; t < 25; t++){
             printf("%3d", B[r][t]);
-            printf(" , ");
+            printf(", ");
         }
-        printf("\n");
+        printf("end \n");
     }
 
     return 0;
